b24ce6c99523a1abcfc90fe782c269c0a84023db
Zikrullo <zikrullogaming@gmail.com>
2025-10-02 16:19:09 +0500

    Update PWA build

--- assets/Logout-Dao1E6-T.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./index-DhKF-2ok.js";const t={name:"Logout",async mounted(){await o.logout(),window.location.href="/login"}};export{t as default};
---
+++ assets/Logout-KDhwqDs7.js
@@ -0,0 +1,1 @@
+import{u as o}from"./index-Y6BNfCJ1.js";const t={name:"Logout",async mounted(){await o.logout(),window.location.href="/login"}};export{t as default};
