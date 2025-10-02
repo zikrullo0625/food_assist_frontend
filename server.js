d02939967c4060dadc85644d2d29c412300c271c
Zikrullo <zikrullogaming@gmail.com>
2025-10-02 17:18:21 +0500

    123

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 
+// Для __dirname в ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Отдаём статику
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// fallback для Vue Router History mode
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
