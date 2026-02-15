3e11fee981794755c951aad9fc20f26a9c0d5a0b
Zikrullo <zikrullogaming@gmail.com>
2026-02-15 23:11:57 +0500

    Updates

--- assets/Logout-Bqui9qm8.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./index-gVmIVoGD.js";const t={name:"Logout",async mounted(){await o.logout(),window.location.href="/login"}};export{t as default};
---
+++ assets/Logout-CWzCZBlV.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-D_MvzuAN.js";const t={name:"Logout",async mounted(){await o.logout(),window.location.href="/login"}};export{t as default};
